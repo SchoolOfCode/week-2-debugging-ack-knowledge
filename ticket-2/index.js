716c6f9f71a57e6d0797a6306539485be4acc6fb
chargrace <lottie808@hotmail.co.uk>
2024-12-04 12:45:43 +0000

    debugging ticket 2

--- ticket-2/index.js
+++ ticket-2/index.js
@@ -1,13 +1,14 @@
-Functin extractInitialsFromFullName(fullName) {
-/** Assumes that each name within the full name is separated by a "space" that can be used as a separator/delimiter.  */
-const         names            = fullName.split(" ");
-let initials = "";
+function extractInitialsFromFullName(fullName) {
+    const names = fullName.split(" ");
+    let initials = "";
+    /** Assumes that each name within the full name is separated by a "space" that can be used as a separator/delimiter.  */
 
-For (let i = 0; i < names.length; i++) [
-Const currentName = names[i];
-const currentInitial = currentName.at(0);
-initials += currentInitial;
+for (let i = 0; i < names.length; i++) {
+    const currentName = names[i];
+    const currentInitial = currentName.at(0);
+    initials += currentInitial;
 }
 
-Return initials;
-)
+return initials;
+
+}
